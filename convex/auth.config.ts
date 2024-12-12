152acbc3645719ae9ebaba52f365024e63e07d01
abhishek <abhishek4code@gmail.com>
2024-12-13 00:48:12 +0530

    feat: clerk with convex integration added && some changes in  convex provider

---
+++ convex/auth.config.ts
@@ -0,0 +1,11 @@
+const authConfig =  {
+    providers: [
+      {
+        domain: "https://grown-glowworm-22.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+};
+
+export default authConfig;
+
