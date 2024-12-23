36c744261cd0a2c22f3081849e77507abc143b32
abhishek <abhishek4code@gmail.com>
2024-12-23 21:36:44 +0530

    podcastdetailplayer ui added

--- app/(main)/podcasts/[podcastID]/page.tsx
+++ app/(main)/podcasts/[podcastID]/page.tsx
@@ -1,7 +1,9 @@
 'use client'
 
+import EmptyPod from '@/components/EmptyPod';
 import LoaderSpinner from '@/components/LoaderSpinner';
 import PodcastCard from '@/components/PodcastCard';
+import PodcastDetailPlayer from '@/components/PodcastDetailPlayer';
 import { api } from '@/convex/_generated/api';
 import { Id } from '@/convex/_generated/dataModel';
 import { useUser } from '@clerk/nextjs'
