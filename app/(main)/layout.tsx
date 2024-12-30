27e95d17df12c790b46dfef509257c5945d22e98
abhishek <abhishek4code@gmail.com>
2024-12-30 23:25:27 +0530

    chore: podcast player added in the layout to avail on all pgs

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -2,6 +2,7 @@ import LeftSidebar from "@/components/LeftSidebar";
 import RightSidebar from "@/components/RightSidebar";
 import Image from "next/image";
 import { Toaster } from "@/components/ui/toaster"
+import PodcastPlayer from "@/components/PodcastPlayer";
 
 export default function RootLayout({
   children,
@@ -27,6 +28,7 @@ export default function RootLayout({
         </div>
         <RightSidebar/>
     </main>
+    <PodcastPlayer/>
    </div>
   );
 }
